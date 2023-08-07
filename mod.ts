67821856531f1f1088a54187295233d667bd9ce1
Ola Næss Kaldestad <mail@okal.no>
2023-08-07 08:04:48 +0200

    re-export echo

--- mod.ts
+++ mod.ts
@@ -1,3 +1,3 @@
 export { default, Router } from "./src/router.ts";
 export { ServerError } from "./src/error.ts";
-export { cors } from "./src/middleware.ts";
+export { cors, echo } from "./src/middleware.ts";
