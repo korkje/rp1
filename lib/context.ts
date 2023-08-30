2ef55906f8f6f3724ab6ffd49026375f46ad3025
Ola Næss Kaldestad <mail@okal.no>
2023-08-30 02:27:14 +0200

    improve parameter typing

--- lib/context.ts
+++ lib/context.ts
@@ -3,9 +3,9 @@ import { ServerError, ServerErrorParams } from "./error.ts";
 type Increment<N extends unknown[]> = [unknown, ...N];
 type Size<T extends unknown[]> = T['length'];
 
-export type ExtractParams<Path extends string> =
+export type ExtractNamedParams<Path extends string> =
     Path extends `${infer _}:${infer Param}/${infer Rest}`
-    ? Param | ExtractParams<Rest>
+    ? Param | ExtractNamedParams<Rest>
     : Path extends `${infer _}:${infer Param}` ? Param : never;
 
 export type ExtractWildcards<Path extends string, Counter extends unknown[] = []> =
@@ -13,11 +13,15 @@ export type ExtractWildcards<Path extends string, Counter extends unknown[] = []
     ? Size<Counter> | ExtractWildcards<Rest, Increment<Counter>>
     : never;
 
+export type Params<Path extends string> =
+    Record<ExtractNamedParams<Path> | ExtractWildcards<Path>, string> &
+    Record<string, string | undefined>;
+
 export class Context<Path extends string = string> {
     public response: Response;
 
     constructor(
-        public params: Record<ExtractParams<Path> | ExtractWildcards<Path>, string>,
+        public params: Params<Path>,
         public request: Request,
         public info: Deno.ServeHandlerInfo,
     ) {
